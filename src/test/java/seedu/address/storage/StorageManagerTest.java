c45c66ec50342b2175abdf7847e842ec0eb9369a
lamchenghou <lamchenghouu@gmail.com>
2023-10-13 14:36:26 +0800

    Update existing JUnit tests

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -25,8 +25,10 @@ public class StorageManagerTest {
     @BeforeEach
     public void setUp() {
         JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(getTempFilePath("ab"));
+        JsonLocalCourseCatalogueStorage localCourseCatalogueStorage =
+                new JsonLocalCourseCatalogueStorage(getTempFilePath("localcourse"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        storageManager = new StorageManager(addressBookStorage, localCourseCatalogueStorage, userPrefsStorage);
     }
 
     private Path getTempFilePath(String fileName) {

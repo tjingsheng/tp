6c1aff9bc6ef37a9c8f32b2a1183fa2b4fb1192e
lamchenghou <lamchenghouu@gmail.com>
2023-10-13 14:34:00 +0800

    Add local course add command

---
+++ src/main/java/seedu/address/logic/commands/LocalCourseCommand.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Abstract class for LocalCourseCommands.
+ */
+public abstract class LocalCourseCommand extends Command {
+
+    public static final String COMMAND_WORD = "localcourse";
+
+    /* The message shown upon a ParseException */
+    private static final String MESSAGE_USAGE = COMMAND_WORD + ": Courses offered by NUS Computing"
+            + "Commands: "
+            + "localcourse list: Lists all available local courses."
+            + "localcourse add [localcode] [localname]: Adds a local course."
+            + "localcourse delete [localcode]";
+
+    protected LocalCourseCommand() {
+    }
+
+    // Ensure that this method is overridden in all subclasses
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+
+    // Ensure that this method is overridden in all subclasses
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).toString();
+    }
+
+}

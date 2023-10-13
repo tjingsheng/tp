6d27ed4bd6cd393f81d4462906c1d93f7422416b
lamchenghou <lamchenghouu@gmail.com>
2023-10-13 14:36:09 +0800

    Update MainApp and add SeplendidLogsCenter

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -15,16 +15,24 @@ import seedu.address.commons.util.ConfigUtil;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
+import seedu.address.logic.SeplendidLogic;
+import seedu.address.logic.SeplendidLogicManager;
 import seedu.address.model.AddressBook;
+import seedu.address.model.LocalCourseCatalogue;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyLocalCourseCatalogue;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.SeplendidModel;
+import seedu.address.model.SeplendidModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
 import seedu.address.storage.JsonAddressBookStorage;
+import seedu.address.storage.JsonLocalCourseCatalogueStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
+import seedu.address.storage.LocalCourseCatalogueStorage;
 import seedu.address.storage.Storage;
 import seedu.address.storage.StorageManager;
 import seedu.address.storage.UserPrefsStorage;
@@ -41,9 +49,15 @@ public class MainApp extends Application {
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
     protected Ui ui;
-    protected Logic logic;
+    // Deprecate after morphing
+    protected Logic ab3Logic;
+    // New logic class
+    protected SeplendidLogic seplendidLogic;
     protected Storage storage;
+    // Deprecate after morphing
     protected Model model;
+    // New model class
+    protected SeplendidModel seplendidModel;
     protected Config config;
 
     @Override
@@ -58,13 +72,20 @@ public class MainApp extends Application {
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
         UserPrefs userPrefs = initPrefs(userPrefsStorage);
         AddressBookStorage addressBookStorage = new JsonAddressBookStorage(userPrefs.getAddressBookFilePath());
-        storage = new StorageManager(addressBookStorage, userPrefsStorage);
+        LocalCourseCatalogueStorage localCourseCatalogueStorage =
+                new JsonLocalCourseCatalogueStorage(userPrefs.getLocalCourseCatalogueFilePath());
 
-        model = initModelManager(storage, userPrefs);
+        storage = new StorageManager(addressBookStorage, localCourseCatalogueStorage, userPrefsStorage);
+        // AB3 model
+        model = initAddressBookModelManager(storage, userPrefs);
+        // SEPlendid model
+        seplendidModel = initSeplendidModelManager(storage, userPrefs);
 
-        logic = new LogicManager(model, storage);
+        ab3Logic = new LogicManager(model, storage);
+        // Developer's note: UI is able to use seplendidLogic to execute commands
+        seplendidLogic = new SeplendidLogicManager(seplendidModel, storage);
 
-        ui = new UiManager(logic);
+        ui = new UiManager(ab3Logic);
     }
 
     /**
@@ -72,7 +93,7 @@ public class MainApp extends Application {
      * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
      * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
      */
-    private Model initModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
+    private Model initAddressBookModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
         logger.info("Using data file : " + storage.getAddressBookFilePath());
 
         Optional<ReadOnlyAddressBook> addressBookOptional;
@@ -91,6 +112,36 @@ public class MainApp extends Application {
         }
 
         return new ModelManager(initialData, userPrefs);
+    }
+
+    /**
+     * Returns a {@code SeplendidModelManager} with the data from {@code storage, LocalCourseCatalogue,
+     * PartnerCourseCatalogue, UniversityCatalogue, NoteCatalogue, userPrefs}.
+     * <br>
+     * Sample data will be used instead if {@code storage}'s data is not found,
+     * or an empty dataset will be used instead if errors occur when reading {@code storage}'s data.
+     */
+    private SeplendidModelManager initSeplendidModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
+        // The below needs to be repeated several times for the different data types
+        logger.info("Using data file : " + storage.getLocalCourseCatalogueFilePath());
+
+        Optional<ReadOnlyLocalCourseCatalogue> localCourseCatalogueOptional;
+        ReadOnlyLocalCourseCatalogue intiialLocalCourseCatalogue;
+        try {
+            localCourseCatalogueOptional = storage.readLocalCourseCatalogue();
+            if (!localCourseCatalogueOptional.isPresent()) {
+                logger.info("Creating a new data file " + storage.getLocalCourseCatalogueFilePath()
+                        + " populated with a sample LocalCourseCatalogue.");
+            }
+            intiialLocalCourseCatalogue = localCourseCatalogueOptional.orElseGet(
+                    SampleDataUtil::getSampleLocalCourseCatalogue);
+        } catch (DataLoadingException e) {
+            logger.warning("Data file at " + storage.getLocalCourseCatalogueFilePath() + " could not be loaded."
+                    + " Will be starting with an empty AddressBook.");
+            intiialLocalCourseCatalogue = new LocalCourseCatalogue();
+        }
+
+        return new SeplendidModelManager(intiialLocalCourseCatalogue, userPrefs);
     }
 
     private void initLogging(Config config) {
@@ -168,6 +219,16 @@ public class MainApp extends Application {
         return initializedPrefs;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The overriding start method for MainApp class. the entry point.
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     */
     @Override
     public void start(Stage primaryStage) {
         logger.info("Starting AddressBook " + MainApp.VERSION);

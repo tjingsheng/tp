05fbe2cffd6d19c7bdceb9fa7d6ddc7096882698
dlathyun <shyim0128@gmail.com>
2023-10-18 12:29:30 +0800

    Edit PartnerCourseAddCommandParser

--- src/main/java/seedu/address/logic/parser/PartnerCourseAddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PartnerCourseAddCommandParser.java
@@ -17,7 +17,7 @@ import seedu.address.model.university.University;
 
 public class PartnerCourseAddCommandParser implements Parser<PartnerCourseAddCommand> {
     public PartnerCourseAddCommand parse(String args) throws ParseException {
-        if (!areValuesEnclosedAndNonEmpty(args)) {
+        if (!ParserUtil.areValuesEnclosedAndNonEmpty(args)) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                             PartnerCourseAddCommand.PARTNER_COURSE_ADD_MESSAGE_USAGE)
@@ -28,7 +28,7 @@ public class PartnerCourseAddCommandParser implements Parser<PartnerCourseAddCom
                 SeplendidArgumentTokenizer.tokenize(args,
                         PARAMETER_UNIVERSITY, PARAMETER_PARTNERCODE, PARAMETER_PARTNERNAME);
 
-        if (!areArgumentsPresent(parameterToArgMap, PARAMETER_UNIVERSITY, PARAMETER_PARTNERCODE, PARAMETER_PARTNERNAME)) {
+        if (!ParserUtil.areArgumentsPresent(parameterToArgMap, PARAMETER_UNIVERSITY, PARAMETER_PARTNERCODE, PARAMETER_PARTNERNAME)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     PartnerCourseAddCommand.PARTNER_COURSE_ADD_MESSAGE_USAGE));
         }
@@ -41,44 +41,5 @@ public class PartnerCourseAddCommandParser implements Parser<PartnerCourseAddCom
         PartnerCourse partnerCourse = new PartnerCourse(university, partnerCode, partnerName);
 
         return new PartnerCourseAddCommand(partnerCourse);
-    }
-
-    /**
-     * Returns true if all arguments are enclosed in square brackets, and are non-empty.
-     *
-     * @param args Arguments in the format of {@code [args1] [args2] ...}.
-     * @return true if in correct format.
-     */
-    private static boolean areValuesEnclosedAndNonEmpty(String args) {
-        int bracketCount = 0;
-        StringBuilder currValue = new StringBuilder();
-
-        for (int i = 0; i < args.length(); i++) {
-            Character currChar = args.charAt(i);
-            if (currChar.equals('[')) {
-                currValue.setLength(0);
-                bracketCount++;
-            } else if (currChar.equals(']') && currValue.toString().trim().isEmpty()) {
-                return false;
-            } else if (currChar.equals(']')) {
-                bracketCount--;
-            } else {
-                currValue.append(currChar);
-            }
-
-            if (bracketCount < 0 || bracketCount > 1) {
-                return false;
-            }
-        }
-
-        return bracketCount == 0;
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code SeplendidArgumentMap}.
-     */
-    private static boolean areArgumentsPresent(SeplendidArgumentMap argumentMap, SeplendidParameter... parameters) {
-        return Stream.of(parameters).allMatch(parameter -> argumentMap.getValue(parameter).isPresent());
     }
 }

03dbb2cdf92cef36e55c88658f640a673f09f270
alyssaongyx <ongyixian@gmail.com>
2023-10-22 17:41:19 +0800

    Add basic test cases

---
+++ src/test/java/seedu/address/logic/parser/UniversitySearchCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.UniversitySearchCommand;
+import seedu.address.model.university.UniversityNameContainsKeywordsPredicate;
+public class UniversitySearchCommandParserTest {
+    private UniversitySearchCommandParser parser = new UniversitySearchCommandParser();
+
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                UniversitySearchCommand.UNIVERSITY_SEARCH_MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsUniversitySearchCommand() {
+        // no leading and trailing whitespaces
+        UniversitySearchCommand expectedUniversitySearchCommand =
+                new UniversitySearchCommand(new UniversityNameContainsKeywordsPredicate(
+                        Arrays.asList("Stanford", "Seoul")));
+        assertParseSuccess(parser, "Stanford Seoul", expectedUniversitySearchCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Stanford \n \t Seoul \t", expectedUniversitySearchCommand);
+    }
+
+}

ba0fe18f4cd5c9c0d49cd19b02727602e0e48692
dlathyun <shyim0128@gmail.com>
2023-10-27 15:57:24 +0800

    Fix errors

--- src/main/java/seedu/address/seplendidui/ItemDetailPanel.java
+++ src/main/java/seedu/address/seplendidui/ItemDetailPanel.java
@@ -4,9 +4,8 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.SeplendidLogic;
 import seedu.address.model.LocalCourseCatalogue;
@@ -24,20 +23,20 @@ public class ItemDetailPanel<SeplendidDataType> extends UiPart<Region> {
     private static final String MESSAGE_MAPPING_ITEM_DETAIL_PANEL_ERROR = "Error with mapping item detail panel.";
     private static final String FXML = "ItemDetailPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ItemDetailPanel.class);
-    @FXML
-    private ListView<SeplendidDataType> itemDetailView;
 
     // For access to catalogues
     private SeplendidLogic seplendidLogic;
 
+    @FXML
+    private VBox itemDetailView;
+
+    // TODO: Decide if detail panel actually needs seplendid logic
     /**
      * Creates a {@code ItemDetailPanel}.
      */
-    public ItemDetailPanel(SeplendidLogic logic) {
+    public ItemDetailPanel(SeplendidLogic seplendidLogic) {
         super(FXML);
-        // Set the cell factory to display item in the ListView
-        itemDetailView.setCellFactory(listView -> new ItemDetailViewCell());
-        seplendidLogic = logic;
+        this.seplendidLogic = seplendidLogic;
     }
 
     /**
@@ -46,32 +45,14 @@ public class ItemDetailPanel<SeplendidDataType> extends UiPart<Region> {
      * @param item The selected item
      */
     public void setItemDetail(SeplendidDataType item) {
-        // Clear the existing items and add the selected item
-        itemDetailView.getItems().clear();
+        // Directly set the item to the container
         if (item != null) {
-            itemDetailView.getItems().add(item);
-        }
-    }
-
-    //TODO: Convert it to single item instead of list, detail panel should always be a single item.
-
-    /**
-     * Custom {@code ListCell} that displays the details of an {@code Item}.
-     */
-    class ItemDetailViewCell extends ListCell<SeplendidDataType> {
-        @Override
-        protected void updateItem(SeplendidDataType item, boolean empty) {
-            super.updateItem(item, empty);
-
-            if (empty || item == null) {
-                setGraphic(null);
-                setText(null);
-            } else if (item instanceof LocalCourse) {
-                setGraphic(new LocalCourseDetail((LocalCourse) item).getRoot());
+            if (item instanceof LocalCourse) {
+                itemDetailView.getChildren().setAll(new LocalCourseDetail((LocalCourse) item).getRoot());
             } else if (item instanceof PartnerCourse) {
-                setGraphic(new PartnerCourseDetail((PartnerCourse) item).getRoot());
+                itemDetailView.getChildren().setAll(new PartnerCourseDetail((PartnerCourse) item).getRoot());
             } else if (item instanceof University) {
-                setGraphic(new UniversityDetail((University) item).getRoot());
+                itemDetailView.getChildren().setAll(new UniversityDetail((University) item).getRoot());
             } else if (item instanceof Mapping) {
                 Mapping mapping = (Mapping) item;
                 // We know that RTT of getLocalCourseCatalogue() is LocalCourseCatalogue.
@@ -83,16 +64,18 @@ public class ItemDetailPanel<SeplendidDataType> extends UiPart<Region> {
                         (PartnerCourseCatalogue) seplendidLogic.getPartnerCourseCatalogue();
                 Optional<PartnerCourse> partnerCourse =
                         partnerCourseCatalogue.getPartnerCourseIfExists(
-                                                mapping.getPartnerCode(), mapping.getUniversityName());
-                if (localCourse.isPresent() && partnerCourse.isPresent()) {
-                    setGraphic(new MappingDetail(mapping,
-                            localCourse.get().getLocalName().getValue(),
-                            localCourse.get().getLocalUnit().toString(),
-                            partnerCourse.get().getPartnerName().toString(),
-                            partnerCourse.get().getPartnerUnit().toString()).getRoot());
-                } else {
+                                mapping.getPartnerCode(), mapping.getUniversityName());
+                if (localCourse.isEmpty() || partnerCourse.isEmpty()) {
                     logger.warning(MESSAGE_MAPPING_ITEM_DETAIL_PANEL_ERROR);
+                    return;
                 }
+
+                itemDetailView.getChildren().setAll(new MappingDetail(
+                        mapping,
+                        localCourse.get().getLocalName().toString(),
+                        localCourse.get().getLocalUnit().toString(),
+                        partnerCourse.get().getPartnerName().toString(),
+                        partnerCourse.get().getPartnerUnit().toString()).getRoot());
             }
         }
     }

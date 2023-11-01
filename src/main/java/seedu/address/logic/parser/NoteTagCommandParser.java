e0065bb1c3444743f4431317c36f9101672245cc
aliciamichellew <aliciamichellew@gmail.com>
2023-11-01 22:18:02 +0800

    Add Note Tag parser

---
+++ src/main/java/seedu/address/logic/parser/NoteTagCommandParser.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.NoteTagCommand;
+import seedu.address.logic.commands.NoteUpdateCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.notes.Content;
+import seedu.address.model.tag.Tag;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PARAMETER_CONTENT;
+import static seedu.address.logic.parser.CliSyntax.PARAMETER_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PARAMETER_TAGS;
+import static seedu.address.logic.parser.ParserUtil.areValuesEnclosedAndNonEmpty;
+
+public class NoteTagCommandParser implements Parser<NoteTagCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the NoteUpdateCommand
+     * and returns a NoteUpdateCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public NoteTagCommand parse(String args) throws ParseException {
+        if (!areValuesEnclosedAndNonEmpty(args)) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                            NoteUpdateCommand.NOTE_UPDATE_MESSAGE_USAGE));
+        }
+
+        SeplendidArgumentMap parameterToArgMap =
+                SeplendidArgumentTokenizer.tokenize(args, PARAMETER_INDEX, PARAMETER_TAGS);
+
+        if (!ParserUtil.areArgumentsPresent(parameterToArgMap, PARAMETER_INDEX, PARAMETER_TAGS)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    NoteUpdateCommand.NOTE_UPDATE_MESSAGE_USAGE));
+        }
+
+        // All arguments should be a non-empty {@code Optional}
+
+        try {
+            Integer noteIndex = Integer.valueOf(parameterToArgMap.getValue(PARAMETER_INDEX).get());
+            Tag newTag = new Tag(parameterToArgMap.getValue(PARAMETER_TAGS).get());
+            return new NoteTagCommand(noteIndex, newTag);
+        } catch (NumberFormatException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    NoteUpdateCommand.NOTE_UPDATE_MESSAGE_USAGE));
+        }
+    }
+}

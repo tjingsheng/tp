5a14232d721c21582d64bd6900949c38f5b3ee77
aliciamichellew <aliciamichellew@gmail.com>
2023-11-01 14:47:25 +0800

    Add Note Delete command and parser

---
+++ src/main/java/seedu/address/logic/commands/NoteDeleteCommand.java
@@ -0,0 +1,81 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.SeplendidModel;
+import seedu.address.model.notes.Note;
+import seedu.address.seplendidui.UiUtil;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Deletes a note to the NoteList.
+ */
+public class NoteDeleteCommand extends NoteCommand {
+
+    // Also available in abstract class LocalCourseCommand to support polymorphism
+    public static final String NOTE_DELETE_MESSAGE_USAGE = COMMAND_WORD
+            + " delete [index]: Deletes a note.";
+    public static final String ACTION_WORD = "delete";
+
+    public static final String MESSAGE_SUCCESS = "Note deleted: %1$s";
+
+    private final Integer noteIndexToDelete;
+
+    /**
+     * Creates a NoteAddCommand to add the specified {@code Note}
+     *
+     * @param note The Note to be added into Storage.
+     */
+    public NoteDeleteCommand(int note) {
+        super();
+        requireNonNull(note);
+        noteIndexToDelete = note;
+    }
+
+    /**
+     * TBD: This stub is to be removed after morphing is complete.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return Nothing.
+     * @throws CommandException Always.
+     */
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("TBD: this is a stub and should be removed after morph.");
+    }
+
+    @Override
+    public CommandResult execute(SeplendidModel seplendidModel) throws CommandException {
+        requireNonNull(seplendidModel);
+
+        Note deletedNote = seplendidModel.deleteNote(noteIndexToDelete);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(deletedNote)),
+                UiUtil.ListViewModel.NOTE_LIST);
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NoteDeleteCommand)) {
+            return false;
+        }
+
+        NoteDeleteCommand otherNoteDeleteCommand = (NoteDeleteCommand) other;
+        return noteIndexToDelete.equals(otherNoteDeleteCommand.noteIndexToDelete);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("noteToDelete", noteIndexToDelete)
+                .toString();
+    }
+}

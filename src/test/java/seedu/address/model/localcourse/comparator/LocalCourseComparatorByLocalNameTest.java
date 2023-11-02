74acc0ef1ae6f3908c44f7421b9ae0aad74b7418
dlathyun <shyim0128@gmail.com>
2023-11-02 22:20:01 +0800

    Add more tests

--- src/test/java/seedu/address/model/localcourse/comparator/LocalCourseComparatorByLocalNameTest.java
+++ src/test/java/seedu/address/model/localcourse/comparator/LocalCourseComparatorByLocalNameTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.localcourse.comparator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalObjects.CS2030S;
 import static seedu.address.testutil.TypicalObjects.MA2001;
 
@@ -16,5 +18,23 @@ public class LocalCourseComparatorByLocalNameTest {
         assertEquals(-1, localCourseComparatorByLocalName.compare(MA2001, CS2030S));
         assertEquals(0, localCourseComparatorByLocalName.compare(CS2030S, CS2030S));
         assertEquals(1, localCourseComparatorByLocalName.compare(CS2030S, MA2001));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        assertTrue(localCourseComparatorByLocalName.equals(new LocalCourseComparatorByLocalName()));
+
+        // same object -> returns true
+        assertTrue(localCourseComparatorByLocalName.equals(localCourseComparatorByLocalName));
+
+        // null -> returns false
+        assertFalse(localCourseComparatorByLocalName.equals(null));
+
+        // different types -> returns false
+        assertFalse(localCourseComparatorByLocalName.equals(1));
+
+        // different values -> returns false
+        assertFalse(localCourseComparatorByLocalName.equals(new LocalCourseComparatorByLocalCode()));
     }
 }
--- src/test/java/seedu/address/model/partnercourse/comparator/PartnerCourseComparatorByUniversityTest.java
+++ src/test/java/seedu/address/model/partnercourse/comparator/PartnerCourseComparatorByUniversityTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.partnercourse.comparator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalObjects.COMP1000;
 import static seedu.address.testutil.TypicalObjects.COMP4000;
 
@@ -15,5 +17,23 @@ public class PartnerCourseComparatorByUniversityTest {
         assertEquals(-1, partnerCourseComparatorByUniversity.compare(COMP4000, COMP1000));
         assertEquals(0, partnerCourseComparatorByUniversity.compare(COMP4000, COMP4000));
         assertEquals(1, partnerCourseComparatorByUniversity.compare(COMP1000, COMP4000));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        assertTrue(partnerCourseComparatorByUniversity.equals(new PartnerCourseComparatorByUniversity()));
+
+        // same object -> returns true
+        assertTrue(partnerCourseComparatorByUniversity.equals(partnerCourseComparatorByUniversity));
+
+        // null -> returns false
+        assertFalse(partnerCourseComparatorByUniversity.equals(null));
+
+        // different types -> returns false
+        assertFalse(partnerCourseComparatorByUniversity.equals(1));
+
+        // different values -> returns false
+        assertFalse(partnerCourseComparatorByUniversity.equals(new PartnerCourseComparatorByPartnerName()));
     }
 }
